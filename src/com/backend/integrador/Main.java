e778e9994b101cc2efb763a02f0fa5f3dd44fde6
camila garcia <milaca1991@hotmail.com>
2023-08-09 23:48:10 -0500

    la aplicacion guarda odontologos pero no lista todos los odontologos

--- src/com/backend/integrador/Main.java
+++ src/com/backend/integrador/Main.java
@@ -1,75 +1,72 @@
-package com.backend.integrador;
-
-import org.apache.log4j.Logger;
-
-import java.sql.*;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        Logger logger = Logger.getLogger(Main.class);
-
-
-
-
-        try {
-                //me conecte
-            Connection connection=null;
-
-                //creo la tabla
-            Statement statement = connection.createStatement();
-            //es lo mismo ejecutar la variable pasandola por parametro que escribiendo toda la sentencia
-            statement.execute(SqlCtes.create);
-
-            //inserto a la tabla
-            statement.execute(SqlCtes.insert);
-
-            //hacer un select para consultar la query
-            ResultSet resultSet = statement.executeQuery(SqlCtes.select);
-
-            //recorrer el resultset
-
-            while (resultSet.next()){
-
-                //selecciono si quiero loggear un int o un string
-                    logger.info(resultSet.getString("dsdsd"));
-            }
-
-
-        }
-        catch (Exception exception){
-            logger.error(exception.getMessage());
-
-        }
-        finally {
-            try {
-               // connection.close();
-            }catch (Exception e){
-
-                logger.error(e.getMessage());
-            }
-
-        }
-
-
-
-    }
-
-//crear el metodo para crear la conexion
-
-
-    public static Connection getConnection() throws ClassNotFoundException, SQLException {
-
-        //indicar driver a usar
-
-        Class.forName("org.h2.Driver");
-
-       return DriverManager.getConnection("jdbc:h2:~/parcial","sa","sa");
-
-
-
-    }
-
-
-}
+//package com.backend.integrador;
+//
+//
+//import com.backend.integrador.Dao.H2connection;
+//import org.apache.log4j.Logger;
+//
+//import java.sql.*;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Logger logger = Logger.getLogger(Main.class);
+//
+//
+//        Connection connection=null;
+//
+//        try {
+//                //me conecte
+//
+//            connection= H2connection.getConnection();
+//
+//                //creo la tabla
+//            Statement statement = connection.createStatement();
+//            //es lo mismo ejecutar la variable pasandola por parametro que escribiendo toda la sentencia
+//            statement.execute(SqlCtes.create);
+//
+//            //inserto a la tabla
+//            statement.execute(SqlCtes.insert);
+//
+//            //hacer un select para consultar la query
+//            ResultSet resultSet = statement.executeQuery(SqlCtes.select);
+//
+//            //recorrer el resultset
+//
+//            while (resultSet.next()){
+//
+//                //selecciono si quiero loggear un int o un string
+//                    logger.info(resultSet.getInt("id")+ "-"+resultSet.getString("nombre")+"-"+resultSet.getString("tipo"));
+//
+//            }
+//
+//
+//        }
+//        catch (Exception exception){
+//            logger.error(exception.getMessage());
+//
+//        }
+//        finally {
+//            try {
+//
+//                connection.close();
+//            }catch (Exception e){
+//
+//                logger.error(e.getMessage());
+//            }
+//
+//        }
+//
+//
+//
+//    }
+//
+////crear el metodo para crear la conexion
+//
+//
+//
+//    //esto va en clase h2Connection en carpeta impl que la contiene dao
+//
+//
+//
+//}
--- src/com/backend/integrador/SqlCtes.java
+++ src/com/backend/integrador/SqlCtes.java
@@ -2,10 +2,10 @@ package com.backend.integrador;
 
 public class SqlCtes {
 
-  public static final String create= "DROP TABLE IF EXIST ; CREATE TABLE (ID INT AUTO_INCREMENT PRIMARY KEY, NOMBRE VARCHAR(50) NOT NULL   ";
-    public static final String insert= "INSERT INTO";
-
-    public static final String delete="SELECT FROM WHERE ID=1";
-    public static final String select= "SELECT*FROM ";
+  public static final String create= "DROP TABLE IF EXISTS ODONTOLOGO ; CREATE TABLE ODONTOLOGO(ID INT AUTO_INCREMENT PRIMARY KEY, NUMEROMATRICULA INT NOT NULL, NOMBRE VARCHAR(50) NOT NULL, APELLIDO VARCHAR(50)NOT NULL)";
+//    public static final String insert= "INSERT INTO ANIMALES (NOMBRE,TIPO) VALUES('Homero','perro'),('Uber','gato')";
+//
+//   // public static final String delete="SELECT FROM WHERE ID=1";
+//     public static final String select= "SELECT*FROM ANIMALES";
 
 }
